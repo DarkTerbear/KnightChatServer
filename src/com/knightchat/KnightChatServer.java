d700f63f6fb08ea9360e98a360d5a7c159c16798
Terrance Li <terranceli.terbear@gmail.com>
2017-08-25 14:52:21 -0700

    Copied over KnightChatServer code

---
+++ src/com/knightchat/KnightChatServer.java
@@ -0,0 +1,143 @@
+package com.knightchat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+
+public class KnightChatServer {
+
+    /**
+     * Server listens on port 1337
+     */
+    private static final int PORT = 1337;
+
+    /**
+     * Set of names in the chatroom. Maintained to prevent name duplicates
+     */
+    private static HashSet<String> names = new HashSet<>();
+
+    /**
+     * Set of all PrintWriters for all clients. Maintained for simpler broadcasting
+     */
+    private static HashSet<PrintWriter> writers = new HashSet<>();
+
+    /**
+     * Application start point. Starts listening on port, spawns handler threads.
+     * @param args
+     */
+    public static void main(String[] args) {
+        boolean serverStarted = false;
+        System.out.println("INFO: Starting KnightChat server on port " + PORT + "...");
+        try {
+            ServerSocket listener = new ServerSocket(PORT);
+            try {
+                while (true) {
+                    if (!serverStarted) {
+                        System.out.println("INFO: KnightChat server successfully started on port " + PORT + "...");
+                        serverStarted = true;
+                    }
+                    new Handler(listener.accept()).start();
+                }
+            } finally {
+                listener.close();
+                System.out.println("INFO: KnightChat server stopped");
+            }
+
+        } catch (IOException e) {
+            System.out.println("FATAL: Server startup failed. Cannot listen on port " + PORT);
+            e.printStackTrace();
+        }
+
+    }
+
+    private static class Handler extends Thread {
+        private String name;
+        private Socket socket;
+        private BufferedReader in;
+        private PrintWriter out;
+
+        /**
+         * Constructs a handler thread, squirreling away the socket.
+         * All the interesting work is done in the run method.
+         */
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        /**
+         * Services this thread's client by repeatedly requesting a
+         * screen name until a unique one has been submitted, then
+         * acknowledges the name and registers the output stream for
+         * the client in a global set, then repeatedly gets inputs and
+         * broadcasts them.
+         */
+        public void run() {
+            try {
+                // Create character streams for the socket.
+                in = new BufferedReader(new InputStreamReader(
+                        socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+
+                // Request a name from this client.  Keep requesting until
+                // a name is submitted that is not already used.  Note that
+                // checking for the existence of a name and adding the name
+                // must be done while locking the set of names.
+                while (true) {
+                    out.println("SUBMITNAME");
+                    name = in.readLine();
+                    if (name == null) {
+                        return;
+                    }
+                    synchronized (names) {
+                        if (!names.contains(name)) {
+                            System.out.println("INFO: User " + name + " has joined. Welcome!");
+                            names.add(name);
+                            break;
+                        }
+                    }
+                }
+
+                // Now that a successful name has been chosen, add the
+                // socket's print writer to the set of all writers so
+                // this client can receive broadcast messages.
+                out.println("NAMEACCEPTED");
+                writers.add(out);
+
+                // Accept messages from this client and broadcast them.
+                // Ignore other clients that cannot be broadcasted to.
+                while (true) {
+                    String input = in.readLine();
+                    if (input == null) {
+                        return;
+                    }
+
+                    for (PrintWriter writer : writers) {
+                        System.out.println("MESSAGE " + name + ": " + input);
+                        writer.println("MESSAGE " + name + ": " + input);
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println(e);
+            } finally {
+                // This client is going down!  Remove its name and its print
+                // writer from the sets, and close its socket.
+                if (name != null) {
+                    System.out.println("INFO: User " + name + " is logging out.");
+                    names.remove(name);
+                } else System.out.println("WARN: User w/o name is logging out");
+                if (out != null) {
+                    writers.remove(out);
+                }
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
